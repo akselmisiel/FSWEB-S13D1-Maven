d4d4bd2e935a8928c39378c2d9743942892945f7
Aksel <akselmisiel@gmail.com>
2024-05-20 16:23:37 +0300

    first

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,22 +6,22 @@ public class Main {
     }
 
     public static boolean shouldWakeUp(boolean isBarking, int clock) {
-        
+        return true;
     }
 
     public static boolean hasTeen(int firstAge, int secondAge, int thirdAge) {
-       
+        return true;
     }
 
     public static boolean isCatPlaying(boolean isSummer, int temp) {
-     
+        return true;
     }
 
     public static double area(double width, double height) {
-       
+        return 1;
     }
 
     public static double area(double radius) {
-       
+        return 1;
     }
 }
